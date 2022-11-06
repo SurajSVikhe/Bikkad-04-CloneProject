a803a8296efa4aeb43793aa013f6580678209fdd
SurajSVikhe <vikhesuraj52@gmail.com>
2022-11-06 17:24:27 +0530

    git first project 1 added

---
+++ gitfirstproject1/src/main/java/com/example/demo/Gitfirstproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitfirstproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gitfirstproject1Application.class, args);
+	}
+
+}
